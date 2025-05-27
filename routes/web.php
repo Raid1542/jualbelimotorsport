606b618dc6fafdaf8419e5011461b361258ad185
Raid_Athallah <raidathalah036@gmail.com>
2025-05-27 13:31:32 +0700

    Tambahan Route

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EditProfilController;
 use App\Http\Controllers\ProfilController;
 use App\Http\Controllers\KeranjangController;
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\ProdukController;
+use App\Http\Controllers\Admin\KonfirmasiPembayaranController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -14,7 +17,18 @@ Route::get('/', function () {
 
 
 # ADMIN #
+Route::prefix('admin')->group(function () {
+    Route::view('/dashboard', 'pages.admin.dashboard')->name('pages.admin.dashboard');
+    Route::view('/riwayat-transaksi', 'admin.riwayat-transaksi')->name('admin.riwayat-transaksi');
+    Route::view('/rekap-penjualan', 'pages.admin.rekap-penjualan')->name('admin.rekap-penjualan');
+    Route::view('/konfirmasi_pembayaran', 'pages.admin.konfirmasi_pembayaran')->name('admin.konfirmasi_pembayaran');
 
+    Route::get('/produk', [ProdukController::class, 'index'])->name('admin.produk.index');  // Menampilkan daftar produk
+    Route::get('/produk/create', [ProdukController::class, 'create'])->name('admin.produk.create');  // Menampilkan form tambah produk
+    Route::post('/produk', [ProdukController::class, 'store'])->name('admin.produk.store');  // Menyimpan produk baru
+    Route::get('/dashboard', [DashboardController::class, 'index']);
+    Route::get('/konfirmasi_pembayaran', [KonfirmasiPembayaranController::class, 'index'])->name('admin.konfirmasi.index');
+});
 
 
 # PEMBELI #
