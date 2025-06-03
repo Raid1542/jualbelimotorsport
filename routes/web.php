8e00643e5c12970e9cf72619bf50617147466f94
Raid_Athallah <raidathallah036@gmail.com>
2025-06-03 15:08:42 +0700

    Edit route

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,87 @@ use App\Http\Controllers\Admin\ProdukController as AdminProdukController;
 use App\Http\Controllers\CheckoutController;
 
 
+/*
+|--------------------------------------------------------------------------
+| Rute Umum (Bisa Diakses Semua)
+|--------------------------------------------------------------------------
+*/
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::view('/welcome', 'welcome');
+
+/*
+|--------------------------------------------------------------------------
+| Rute Autentikasi
+|--------------------------------------------------------------------------
+*/
+// Login
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [LoginController::class, 'login']);
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+
+// Register
+Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
+Route::post('/register', [RegisterController::class, 'register']);
+
+
+/*
+|--------------------------------------------------------------------------
+| Rute Pembeli (Harus Login)
+|--------------------------------------------------------------------------
+*/
+
+// Produk
+    Route::get('/produk', [ProdukController::class, 'index'])->name('produk');
+    Route::get('/produk/{id}', [ProdukController::class, 'detail'])->name('produk.detail');
+    Route::get('/produk/kategori/{id}', [ProdukController::class, 'kategori'])->name('');
+
+     // Keranjang
+    Route::get('/keranjang', [KeranjangController::class, 'index'])->name('keranjang.index');
+    Route::get('/keranjang/tambah/{id}', [KeranjangController::class, 'tambah'])->name('keranjang.tambah');
+    Route::delete('/keranjang/hapus/{id}', [KeranjangController::class, 'hapus'])->name('keranjang.hapus');
+
+Route::middleware('auth')->group(function () {
+    // Dashboard Pembeli
+    Route::get('/dashboard', [DashboardPembeliController::class, 'index'])->name('dashboard');
+
+    // Profil
+    Route::get('/profil', [ProfilController::class, 'profil'])->name('profil');
+    Route::get('/edit_profil', [EditProfilController::class, 'edit_profil'])->name('edit_profil');
+
+    // Keranjang
+    //Route::get('/keranjang', [KeranjangController::class, 'index'])->name('keranjang.index');
+    //Route::get('/keranjang/tambah/{id}', [KeranjangController::class, 'tambah'])->name('keranjang.tambah');
+    //Route::delete('/keranjang/hapus/{id}', [KeranjangController::class, 'hapus'])->name('keranjang.hapus');
+
+    // Checkout
+    Route::get('/checkout/{id}', [CheckoutController::class, 'index'])->name('checkout');
+    Route::post('/checkout/proses', [CheckoutController::class, 'proses'])->name('checkout.proses');
+});
+
+
+/*
+|--------------------------------------------------------------------------
+| Rute Admin (Prefix: /admin)
+|--------------------------------------------------------------------------
+*/
+Route::prefix('admin')->name('admin.')->group(function () {
+    // Dashboard
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+    // Riwayat transaksi & Rekap penjualan (hanya view)
+    Route::view('/riwayat-transaksi', 'admin.riwayat-transaksi')->name('riwayat-transaksi');
+    Route::view('/rekap-penjualan', 'pages.admin.rekap-penjualan')->name('rekap-penjualan');
+
+    // Konfirmasi Pembayaran
+    Route::get('/konfirmasi_pembayaran', [KonfirmasiPembayaranController::class, 'index'])->name('konfirmasi.index');
+
+    // Produk (CRUD)
+    Route::get('/produk', [AdminProdukController::class, 'index'])->name('produk.index');
+    Route::get('/produk/create', [AdminProdukController::class, 'create'])->name('produk.create');
+    Route::post('/produk/store', [AdminProdukController::class, 'store'])->name('produk.store');
+    Route::get('/produk/{id}/edit', [AdminProdukController::class, 'edit'])->name('produk.edit');
+    Route::put('/produk/{id}/update', [AdminProdukController::class, 'update'])->name('produk.update');
+    Route::delete('/produk/{id}/delete', [AdminProdukController::class, 'destroy'])->name('produk.destroy');
+});
+
+
