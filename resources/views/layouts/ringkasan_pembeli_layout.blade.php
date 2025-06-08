e549716766a00ef2dec34e6bcc49c2b653ab2b93
Raid_Athallah <raidathallah036@gmail.com>
2025-06-08 23:22:22 +0700

    Perbaiki bagian ringkasan pembelian

--- resources/views/layouts/ringkasan_pembeli_layout.blade.php
+++ resources/views/layouts/ringkasan_pembeli_layout.blade.php
@@ -1,12 +1,11 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="id">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 </head>
-<body>
-    
+<body class="bg-gray-100 text-black">
+    @yield('content')
 </body>
 </html>
