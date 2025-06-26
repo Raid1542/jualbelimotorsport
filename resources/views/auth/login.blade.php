0ec1058f2b4c5ff552608f01225e8c35a71188b1
Dion <dionaldisionyosua71@gmail.com>
2025-06-26 09:05:56 +0700

    Tambahan role di login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -48,6 +48,17 @@
         </button>
       </div>
 
+      <div class="mb-4">
+        <label for="role" class="block text-sm font-medium text-gray-700 mb-1">Login sebagai</label>
+        <select name="role" id="role" required
+          class="w-full px-4 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-yellow-400 bg-white text-gray-900">
+          <option value="">-- Pilih Role --</option>
+          <option value="pembeli" {{ old('role') == 'pembeli' ? 'selected' : '' }}>Pembeli</option>
+          <option value="penjual" {{ old('role') == 'penjual' ? 'selected' : '' }}>Penjual</option>
+        </select>
+      </div>
+
+
       <button type="submit"
         class="w-full py-3 rounded-lg bg-[#ffbf29] text-white font-semibold hover:bg-yellow-400 transition duration-300">
         Masuk
