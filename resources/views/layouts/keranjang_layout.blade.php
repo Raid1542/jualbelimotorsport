6b616657c2a35927ce98081ba58a08099ae83f6b
Raid Aqil <raidathallah036@gmail.com>
2025-07-04 14:30:58 +0700

    Edit keranjang

--- resources/views/layouts/keranjang_layout.blade.php
+++ resources/views/layouts/keranjang_layout.blade.php
@@ -1,15 +1,19 @@
 <!DOCTYPE html>
 <html lang="id">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>@yield('title', 'SpeedZone')</title>
   <script src="https://cdn.tailwindcss.com"></script>
   <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" rel="stylesheet">
+  <!-- SweetAlert2 -->
+  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </head>
+
 <body class="bg-gray-100">
 
- @include('components.keranjang')
+  @include('components.keranjang')
 
   {{-- Konten --}}
   <main>
@@ -20,4 +24,5 @@
   @include('components.footer')
 
 </body>
+
 </html>
